81f3b4569296d07bab74b52431ca24eddce57435
Andrey Zablotskiy <zablotskyandrew@yandex.ru>
2025-10-24 13:05:58 +0400

    Создана модель Магазина - Shop

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Shop(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Shop"
+        verbose_name_plural = "Shops"
+        ordering = ['name']
+    
