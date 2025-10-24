3827c5329eee96f3e93f1dff9a006172c68a6ce4
Andrey Zablotskiy <zablotskyandrew@yandex.ru>
2025-10-24 15:05:13 +0400

    Создана модель Категории (m2m) с Магазином

--- backend/models.py
+++ backend/models.py
@@ -10,9 +10,27 @@ class Shop(models.Model):
     class Meta:
         verbose_name = "Магазин"
         verbose_name_plural = "Список магазинов"
-        ordering = ['-name']
+        ordering = ["name"]
 
     def __str__(self):
         return self.name
     
     
+class Category(models.Model):
+    """Модель Категории товаров"""
+
+    name = models.CharField(max_length=255, verbose_name="Название категории")
+    description = models.TextField(blank=True, verbose_name="Описание категории")
+    shops = models.ManyToManyField(Shop, related_name="categories", verbose_name="Магазины")
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Список категорий"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+
+
