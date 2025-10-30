2ec7a769290fe5ec54ef06b4832b4aef90f54d35
Andrey Zablotskiy <zablotskyandrew@yandex.ru>
2025-10-30 14:56:33 +0400

    Изменил на Debug=True для отображения админки

--- orders/settings.py
+++ orders/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kcpk8wo=31u50_8d+)ovs##q@=vqjs1btnbafeuxygy)qyu9t%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
