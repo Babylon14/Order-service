83e71c4e51f2464b61733ba44a0d8b5e844237e2
Andrey Zablotskiy <zablotskyandrew@yandex.ru>
2025-11-03 12:44:58 +0400

    Добавлен маршрут для регистрации -register/

--- backend/api/v1/urls.py
+++ backend/api/v1/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("import_shop/<int:shop_id>/", views.import_shop_data_api, name="import_shop_data_api_v1"),
     path("import_all_shops/", views.import_all_shops_data_api, name="import_all_shops_data_api_v1"),
+    path("register/", views.UserRegistrationAPIView.as_view(), name="user_registration_api_v1"),
 ]
 
 
