9b1bbcc4266f8c10cd3032925241592750182c09
Andrey Zablotskiy <zablotskyandrew@yandex.ru>
2025-11-03 13:21:49 +0400

    Исправление ошибок в auth_views.py

--- backend/api/v1/urls.py
+++ backend/api/v1/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from . import views
+from . import auth_views, views
 
 
 urlpatterns = [
     path("import_shop/<int:shop_id>/", views.import_shop_data_api, name="import_shop_data_api_v1"),
     path("import_all_shops/", views.import_all_shops_data_api, name="import_all_shops_data_api_v1"),
-    path("register/", views.UserRegistrationAPIView.as_view(), name="user_registration_api_v1"),
+    path("register/", auth_views.UserRegistrationAPIView.as_view(), name="user_registration_api_v1"),
 ]
 
 
