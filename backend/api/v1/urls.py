351d7306283ef4003f5ab7a50ea8c16a63bcea08
Andrey Zablotskiy <zablotskyandrew@yandex.ru>
2025-11-03 14:25:28 +0400

    Добавлен маршрут для аутентификации пользователя

--- backend/api/v1/urls.py
+++ backend/api/v1/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("import_shop/<int:shop_id>/", views.import_shop_data_api, name="import_shop_data_api_v1"),
     path("import_all_shops/", views.import_all_shops_data_api, name="import_all_shops_data_api_v1"),
     path("register/", auth_views.UserRegistrationAPIView.as_view(), name="user_registration_api_v1"),
+    path("login/", auth_views.UserLoginAPIView.as_view(), name="user_login_api_v1"), 
 ]
 
 
