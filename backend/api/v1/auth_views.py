93e1a37855a3ceb261c4403997f60fa534c89b1b
Andrey Zablotskiy <zablotskyandrew@yandex.ru>
2025-11-03 15:24:23 +0400

    Использовал TokenObtainPairView для генерации токена и изменил вьюшку UserLoginAPIVIew. зарегистрировал маршруты для login/ и  обновления токена token/refresh/

--- backend/api/v1/auth_views.py
+++ backend/api/v1/auth_views.py
@@ -2,9 +2,10 @@ from rest_framework import status
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from django.contrib.auth import login
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
-from backend.api.auth_serializers import UserRegistrationSerializer, UserLoginSerializer
+from backend.api.auth_serializers import UserRegistrationSerializer
 from backend.models import User
 
 
@@ -17,7 +18,7 @@ class UserRegistrationAPIView(CreateAPIView):
     serializer_class = UserRegistrationSerializer
     permission_classes = [AllowAny]  # Разрешаем всем (включая неавторизованных) пользователям регистрироваться
 
-    def create(self, request, *args, **kwargs):
+    def create(self, request):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
@@ -27,24 +28,36 @@ class UserRegistrationAPIView(CreateAPIView):
         )
 
 
-class UserLoginAPIView(CreateAPIView):
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    """Кастомный сериализатор для получения JWT, чтобы при ВХОДЕ использовать email вместо username"""
+    username_field = "email" # Поле для входа
+
+    def validate(self, attrs):
+        """Переопределяем метод validate, чтобы использовать email и пароль для аутентификации"""
+        attrs["username"] = attrs.get("email")
+        
+        # Вызываем родительский метод для стандартной валидации и получения токенов
+        data = super().validate(attrs)
+
+        # Добавляем дополнительную информацию в ответ
+        user = self.user
+        data["user_info"] = {
+            "id": user.id,
+            "username": user.username,
+            "email": user.email,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+        }
+        return data
+    
+
+class UserLoginAPIView(TokenObtainPairView):
     """
-    API View для аутентификации(входа) пользователя.
+    API View для аутентификации(входа) пользователя с использованием JWT.
     POST /api/v1/login/
+    Ожидает 'email' и 'password'.
+    Возвращает 'access' и 'refresh' токены.
     """
-    serializer_class = UserLoginSerializer
+    serializer_class = CustomTokenObtainPairSerializer
     permission_classes = [AllowAny]  # Разрешаем всем (включая неавторизованных) пользователям входить
 
-    def post(self, request):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data["user"]
-
-        # Логиним пользователя (создаем сессию)
-        login(request, user)
-
-        return Response(
-            {"status": "успешно", "message": f"Пользователь {user.username} успешно вошел в систему."},
-            status=status.HTTP_200_OK
-        )
-        
