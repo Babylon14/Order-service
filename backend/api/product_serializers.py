76a52c5a6bb2850324b75e61aeb0579ae3b206b6
Andrey Zablotskiy <zablotskyandrew@yandex.ru>
2025-11-04 15:01:02 +0400

    Добавлен сериализатор для подробной информации о товаре

--- backend/api/product_serializers.py
+++ backend/api/product_serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from backend.models import ProductParameter
+from backend.models import ProductParameter, ProductInfo
 
 
 class ProductParameterSerializer(serializers.ModelSerializer):
@@ -10,3 +10,16 @@ class ProductParameterSerializer(serializers.ModelSerializer):
         model = ProductParameter
         fields = ["parameter_name", "value"]  # Включаем имя параметра и его значение
 
+
+class ProductInfoSerializer(serializers.ModelSerializer):
+    """Сериализатор для информации о товаре (цена, количество, магазин, параметры)."""
+    shop_name = serializers.CharField(source="shop.name", read_only=True)
+    product_parameters = ProductParameterSerializer(
+        source="product_parameters.all", many=True, read_only=True)
+
+    class Meta:
+        model = ProductInfo
+        fields = [
+            "id", "shop_name", "price",
+            "quantity", "product_parameters"
+        ]  # Включаем магазин, цену, кол-во, параметры
