a5f71f12b5dd22055b2c7ab26b51b99205702599
Andrey Zablotskiy <zablotskyandrew@yandex.ru>
2025-11-05 12:55:28 +0400

    Добавление сариализаторов для харак-тик товара и инфо о товаре

--- backend/api/product_serializers.py
+++ backend/api/product_serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from backend.models import ProductParameter, ProductInfo, Product
 
 
+# --- СЕРИАЛИЗАТОРЫ ДЛЯ ПРОДУКТОВ ---
 class ProductParameterSerializer(serializers.ModelSerializer):
     """Сериализатор для характеристик (параметров) товара."""
     parameter_name = serializers.CharField(source="parameter.name", read_only=True)
@@ -11,6 +12,37 @@ class ProductParameterSerializer(serializers.ModelSerializer):
         fields = ["parameter_name", "value"]  # Включаем имя параметра и его значение
 
 
+class ProductInfoSerializer(serializers.ModelSerializer):
+    """Сериализатор для информации о товаре (цена, количество, магазин, параметры)."""
+    shop_name = serializers.CharField(source="shop.name", read_only=True)
+    product_parameters = ProductParameterSerializer(
+        source="product_parameters.all", many=True, read_only=True)
+    # name из ProductInfo интерпретируется как "описание"
+    description = serializers.CharField(source="name", read_only=True)
+
+    class Meta:
+        model = ProductInfo
+        fields = [
+            "id", "shop_name", "price",
+            "quantity", "product_parameters", "description"
+        ]
+
+class ProductListSerializer(serializers.ModelSerializer):
+    """Сериализатор для списка/деталей товаров (включает список ProductInfo)."""
+    category_name = serializers.CharField(source="category.name", read_only=True)
+    product_infos = ProductInfoSerializer(many=True, read_only=True) # Включаем информацию о товаре (цены, магазоны и т.д.)
+
+    class Meta:
+        model = Product
+        fields = [
+            "id",
+            "name",
+            "category_name",
+            "product_infos",
+        ]
+
+
+# --- СЕРИАЛИЗАТОР ДЛЯ СПИСКА ProductInfo (для ProductInfoListView) ---
 class ProductInfoListSerializer(serializers.ModelSerializer):
     """Сериализатор для информации о товаре (цена, количество, магазин)."""
     product_name = serializers.CharField(source="product.name", read_only=True)
@@ -34,5 +66,3 @@ class ProductInfoListSerializer(serializers.ModelSerializer):
             "quantity", # Количество товара
         ]
 
-
-  
