0ac153e66d042d8b44043a9da0655bc0a796ef9c
Andrey Zablotskiy <zablotskyandrew@yandex.ru>
2025-11-07 06:47:20 +0400

    Добавлен модуль сериализатора корзины и позиции в корзине

---
+++ backend/api/cart_serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+from backend.models import Cart, CartItem
+
+
+# --- СЕРИАЛИЗАТОРЫ ДЛЯ КОРЗИНЫ ---
+
+class CartItemSerializer(serializers.ModelSerializer):
+    """Сериализатор для позиции в корзине."""
+
+    product_name = serializers.CharField(source="product_info.product.name", read_only=True)
+    shop_name = serializers.CharField(source="product_info.shop.name", read_only=True)
+    price = serializers.DecimalField(
+        source='product_info.price', max_digits=10, decimal_places=2, read_only=True
+    )
+    total_price = serializers.SerializerMethodField()
+
+    class Meta:
+        model = CartItem
+        fields = ["id", "product_name", "product_shop", "price", "quantity", "total_price"]
+
+    def get_total_price(self, obj):
+        """Вычисляет и возвращает общую сумму для этой позиции."""
+        return float(obj.get_total_price())
+
+
+class CartSerializer(serializers.ModelSerializer):
+    """Сериализатор для Корзины"""
+
+    items = CartItemSerializer(many=True, read_only=True)
+    total_price = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Cart
+        fields = ["id", "items", "total_price", "updated_at"]
+
+    def get_total_price(self, obj):
+        """Вычисляет и возвращает общую сумму для этой позиции."""
+        return float(obj.get_total_price())
+
+
+
