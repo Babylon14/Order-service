0b50a65833ab98455d71bd5451b692d473759e77
Andrey Zablotskiy <zablotskyandrew@yandex.ru>
2025-11-11 15:50:24 +0400

    Создан модуль order_serializers.py, куда добавлен сериализатор для позиции в заказе.

---
+++ backend/api/order_serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from backend.models import Order, OrderItem
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    """Сериализатор для позиции в заказе."""
+    product_name = serializers.CharField(source="product_info.product.name", read_only=True)
+    shop_name = serializers.CharField(source="product_info.shop.name", read_only=True)
+    price = serializers.DecimalField(
+        source="product_info.price", max_digits=10, decimal_places=2, read_only=True
+    )
+
+    class Meta:
+        model = OrderItem
+        fields = ["id", "product_name", "shop_name", "price", "quantity"]
+
