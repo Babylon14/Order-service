cd4fc6507f6a1527b8e53f6fa0ebdca30c77f02b
Andrey Zablotskiy <zablotskyandrew@yandex.ru>
2025-11-19 15:59:12 +0400

    Добавлена логика для импорта данных ВСЕХ магазинов.

--- backend/tasks.py
+++ backend/tasks.py
@@ -76,4 +76,50 @@ def import_shop_data_logic(shop_id: int, yaml_file_path: str = None) -> dict:
     return load_shop_data_from_yaml(shop_id=shop.id, yaml_file_path=yaml_file_path)
 
 
+def import_all_shops_data_logic() -> dict:
+    """
+    Синхронная логика для импорта данных *ВСЕХ* активных магазинов.
+    Не зависит от Celery или DRF.
+    """
+    shops = Shop.objects.filter(state=True) # Получаем список АКТИВНЫХ магазинов
+
+    results_list = []
+    success_count = 0
+    error_count = 0
+
+    for shop in shops:
+        yaml_file_path = shop.get_source_file_path() # Получаем путь к YAML-файлу
+
+        # Вызываем синхронную логику импорта для одного конкретного магазина
+        result = import_shop_data_logic(shop.id, yaml_file_path)
+        if result and result.get("status") == "success": # Если импорт прошел успешно
+            success_count += 1
+            results_list.append({
+                "shop_id": shop.id,
+                "shop_name": shop.name,
+                "status": "success",
+                "details": result.get("message", "")
+            })
+        else:
+            # Если импорт не прошел успешно
+            error_count += 1
+            error_message = result.get( 
+                "message", "Ошибка при загрузке данных."
+            ) if result else "Неизвестная ошибка при импорте данных." 
+            results_list.append({
+                "shop_id": shop.id,
+                "shop_name": shop.name,
+                "status": "error",
+                "details": error_message
+            })
+    ovarall_status = "Частично успешно" if success_count > 0 and error_count > 0 \
+        else ("Успешно" if success_count > 0 else "Ошибка")
+
+    return {
+        "overall_status": ovarall_status,
+        "summary": f"Загружено {success_count} магазинов из {len(shops)}, Ошибок: {error_count}",
+        "results": results_list
+    }
+
+
 # --- CELERY ЗАДАЧИ (вызывают синхронную логику) ---
