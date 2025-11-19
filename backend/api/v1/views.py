a309e1fa6f0e999fdcc97868585c577a96665d42
Andrey Zablotskiy <zablotskyandrew@yandex.ru>
2025-11-19 16:32:07 +0400

    Добавлен API View класс для получения статуса выполнения задачи импорта данных.

--- backend/api/v1/views.py
+++ backend/api/v1/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from django.shortcuts import get_object_or_404
+from celery.result import AsyncResult
 
 from backend.models import Shop
 from backend.tasks import import_all_shops_data_task, import_shop_data_task
@@ -38,81 +39,43 @@ class StartImportShopView(APIView):
         ) # Возвращаем id задачи
 
 
+# --- Класс для получения статуса задачи ---
+class GetImportStatusView(APIView):
+    """
+    API View для получения статуса асинхронной задачи импорта.
+    GET /api/v1/import-status/<task_id>/
+    """
+    permission_classes = [IsAuthenticated]
 
-# # --- API Views для импорта данных из YAML-файлов ---
-# @api_view(["POST"])
-# def import_shop_data_api(request: Request, shop_id=None) -> Response:
-#     """
-#     API View для запуска импорта данных магазина из YAML-файла.
-#     Может принимать shop_id в URL или в теле запроса.
-#     """
-#     request_shop_id = shop_id
-#     if request_shop_id is None:
-#         request_shop_id = request.data.get("shop_id")
+    def get(self, request, task_id, *args, **kwargs):
+        task_result = AsyncResult(task_id)
+        
+        if task_result.state == "PENDING": # Если задача еще не запущена
+            response = {
+                "state": task_result.state,
+                "status": "Задача ещё не запущена..."
+            }
+        elif task_result.state == "PROGRESS": # Если задача выполняется
+            response = {
+                "state": task_result.state,
+                "status": "Задача выполняется..."
+            }
+        elif task_result.state == "SUCCESS": # Если задача завершена успешно
+            response = {
+                "state": task_result.state,
+                "status": "Задача завершена успешно",
+                "message": task_result.result
+            }
+        elif task_result.state == "FAILURE": # Если задача завершена с ошибкой
+            response = {
+                "state": task_result.state,
+                "status": "Задача завершена с ошибкой",
+                "message": str(task_result.info)
+            }
+        else: # Если другие состояния
+            response = {
+                "state": task_result.state,
+                "status": str(task_result.info),
+            }
 
-#     if request_shop_id is None:
-#         return Response(
-#             {"status": "error", "message": "shop_id не предоставлен."},
-#             status=status.HTTP_400_BAD_REQUEST
-#         )
-#     try:
-#         shop = Shop.objects.get(id=request_shop_id)
-#     except ObjectDoesNotExist:
-#         return Response(
-#             {"status": "error", "message": f"Магазин с ID {request_shop_id} не найден."},
-#             status=status.HTTP_404_NOT_FOUND
-#         )
-#     yaml_file_path = shop.get_source_file_path()
-#     result = load_shop_data_from_yaml(shop_id=shop.id, yaml_file_path=yaml_file_path)
-#     if result and result.get("status") == "success":
-#         return Response(result, status=status.HTTP_200_OK)
-#     else:
-#         error_message = result.get(
-#             "message", "Неизвестная ошибка при импорте данных."
-#             ) if result else "Неизвестная ошибка при импорте данных."
-#         return Response(
-#             {"error": error_message},
-#             status=status.HTTP_500_INTERNAL_SERVER_ERROR
-#         )
-    
-# @api_view(["POST"])
-# def import_all_shops_data_api(request: Request) -> Response:
-#     """
-#     API View для запуска импорта данных *всех* магазинов из их YAML-файлов.
-#     """
-#     shops = Shop.objects.filter(state=True)
-#     results = []
-#     success_count = 0
-#     error_count = 0
-
-#     for shop in shops:
-#         yaml_file_path = shop.get_source_file_path()
-#         result = load_shop_data_from_yaml(shop_id=shop.id, yaml_file_path=yaml_file_path)
-#         if result and result.get("status") == "success":
-#             success_count += 1
-#             results.append({
-#                 "shop_id": shop.id,
-#                 "shop_name": shop.name,
-#                 "status": "success",
-#                 "details": result.get("message", "")
-#             })
-#         else:
-#             error_count += 1
-#             error_message = result.get(
-#                 "message", "Ошибка при импорте данных."
-#             ) if result else "Неизвестная ошибка при импорте данных."
-#             results.append({
-#                 "shop_id": shop.id,
-#                 "shop_name": shop.name,
-#                 "status": "error",
-#                 "details": error_message
-#             })
-#     overall_status = "partial_success" if success_count > 0 and error_count > 0 else (
-#         "success" if success_count > 0 else "error"
-#     )
-#     return Response({"overall_status": overall_status,
-#         "summary": f'Загружено: {success_count}, Ошибок: {error_count}',
-#         "results": results
-#     }, status=status.HTTP_200_OK if success_count > 0 else status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-
+        return Response(response)
